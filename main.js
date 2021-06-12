3bc0332864b911a2a13f6ad1c3f3257bc7bdd5c6
kimc0de <kimngan.ledang@gmail.com>
2021-06-12 16:27:25 +0200

    challenge

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
-            inventory: 0
+            inventory: 0,
+            onSale: true
         }
     }
 })
