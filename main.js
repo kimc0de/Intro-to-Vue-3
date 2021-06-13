5fc538790df760efcce133555ccd2e05eaad70ea
kimc0de <kimngan.ledang@gmail.com>
2021-06-13 17:03:17 +0200

    finish

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart: 0,
+            cart: [],
             premium: true
         }
     },
-    methods: {}
+    methods: {
+        updateCart(id) {
+            return this.cart.push(id);
+        }
+    }
 })
